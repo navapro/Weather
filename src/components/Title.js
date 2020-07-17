c3c16675a1d8dba57a72f4910127894720c529e9
NK <45469562+navapro@users.noreply.github.com>
2020-07-17 10:46:54 -0600

    Create Title Componet

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>Weather App</h1>
+            <p>Find out weather conditions outside.</p>
+        </div>
+        );
+    }
+}
+
+export default Titles;
