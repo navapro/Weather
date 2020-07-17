e7fb9bd5109d2b4fbd061e2ea9aa5f28dd711b62
NK <45469562+navapro@users.noreply.github.com>
2020-07-17 13:27:10 -0600

    Added api key and api call

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Title from "./components/Title"
 import Form from "./components/Form"
 import Weather from "./components/Weather"
 
+const API_KEY = "a2b6fcd38dafb5cebe5e38cc8b1bf4a0";
+
+
 class App extends React.Component {
+
+  getWeather = async () => {
+    let city = "Manchester";
+    let country = "UK";
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    
+    const data = await api_call.json();
+
+    console.log(data);
+  }
+
   render(){
     return(
       <div> 
