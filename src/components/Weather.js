77d1ce50da182522869c224bb02f32909d066534
NK <45469562+navapro@users.noreply.github.com>
2020-07-18 14:15:08 -0600

    Added error check

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-class Weather extends React.Component{
-    render(){
-        return(
+class Weather extends React.Component {
+    render() {
+        return (
             <div>
-                {this.props.city}
-                {this.props.country}
-                {this.props.temperature}
-                {this.props.humidity}
-                {this.props.description}
-                {this.props.error}
+
+                {this.props.city && this.props.country && <p>Location: {this.props.city},{this.props.country}</p>}
+                {this.props.temperature && <p>temperature: {Math.round(this.props.temperature)}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
+                {this.props.error && <p>{this.props.error}</p>}
             </div>
         );
     }
