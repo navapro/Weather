29a0194290f7da484e1ade0dd04fd76e69732bc8
NK <45469562+navapro@users.noreply.github.com>
2020-07-18 14:15:19 -0600

    Added error check

--- src/App.js
+++ src/App.js
@@ -25,18 +25,28 @@ class App extends React.Component {
     let country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data);
+    console.log(data)
+    if (data.name && data.sys.country){
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
 
-    this.setState({
+      });
+    }else{
 
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-
-    });
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the name of your city and country."
+      });
+    }
   }
 
   render(){
