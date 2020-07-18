19e3a3c7c9de49f3b72a2e6b6b168c7ee75e98c5
NK <45469562+navapro@users.noreply.github.com>
2020-07-18 14:31:44 -0600

    Updated Titles component to be statless element

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-class Titles extends React.Component{
-    render(){
-        return(
-        <div>
-            <h1>Weather App</h1>
-            <p>Find out weather conditions outside.</p>
-        </div>
-        );
-    }
-}
+const Titles = () => (
+    <div>
+        <h1>Weather App</h1>
+        <p>Find out weather conditions outside.</p>
+    </div>
+);
 
 export default Titles;
