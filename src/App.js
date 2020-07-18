b1feb1c28702c79c93549c46da8faaa3f46379ab
NK <45469562+navapro@users.noreply.github.com>
2020-07-18 10:45:52 -0600

    Create a state object

--- src/App.js
+++ src/App.js
@@ -8,16 +8,26 @@ const API_KEY = "a2b6fcd38dafb5cebe5e38cc8b1bf4a0";
 
 
 class App extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+
+  }
   
   getWeather = async (e) => {
     e.preventDefault();
     let city = e.target.elements.city.value;
     let country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    
     const data = await api_call.json();
-
     console.log(data);
+
+    
   }
 
   render(){
@@ -25,7 +35,9 @@ class App extends React.Component {
       <div> 
         <Title />
         <Form getWeather = {this.getWeather}/>
-        <Weather />
+        <Weather
+          
+        />
       </div>
     );
   }
