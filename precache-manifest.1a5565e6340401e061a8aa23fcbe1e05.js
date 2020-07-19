8e70aa0b2051f79749dea65911c7a0f14c43a1f7
NK <45469562+navapro@users.noreply.github.com>
2020-07-18 19:19:17 -0600

    Updates

---
+++ precache-manifest.1a5565e6340401e061a8aa23fcbe1e05.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79d43a7e8e9e85774452c3486c4892a",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "e1e5e4696863d34dcefa",
+    "url": "/Weather/static/css/2.9d0ed34d.chunk.css"
+  },
+  {
+    "revision": "ab46ef4331b8d548a040",
+    "url": "/Weather/static/css/main.95661190.chunk.css"
+  },
+  {
+    "revision": "e1e5e4696863d34dcefa",
+    "url": "/Weather/static/js/2.37513663.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.37513663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab46ef4331b8d548a040",
+    "url": "/Weather/static/js/main.8b67f6bb.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Weather/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Weather/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Weather/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Weather/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Weather/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Weather/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
