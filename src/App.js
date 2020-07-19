743174bbdaa34edf728cbb5a84ba1f1e0e67a6d2
NK <45469562+navapro@users.noreply.github.com>
2020-07-18 20:15:56 -0600

    http to https

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
     e.preventDefault();
     let city = e.target.elements.city.value;
     let country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data)
     if (data.name && data.sys.country){
